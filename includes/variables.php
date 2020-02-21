319551cd6f607489abc2550449bdf59529cf0f70
luizkim <luizkim@users.noreply.github.com>
2020-02-21 13:32:14 -0300

    Update variables.php

--- includes/variables.php
+++ includes/variables.php
@@ -11,7 +11,7 @@ $pluginSupports = array(
 $fc_errors = array();
 
 /*Variáveis globais*/
-$url_shipping_quote = "https://api.freteclick.com.br/sales/shipping-quote.json";
+$url_shipping_quote = "https://app.freteclick.com.br/sales/shipping-quote.json";
 $url_origin_company = "https://app.freteclick.com.br/sales/add-quote-origin-company.json";
 $url_destination_client = "https://app.freteclick.com.br/sales/add-quote-destination-client.json";
 
